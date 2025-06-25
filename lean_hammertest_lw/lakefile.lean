55e0df104c9ef25cba784cee6043f7b0c6b5f9d6
Jannis Limperg <limperg@amazon.com>
2025-06-25 15:55:45 +0200

    Update to v4.20.0

--- lean_hammertest_lw/lakefile.lean
+++ lean_hammertest_lw/lakefile.lean
@@ -7,12 +7,12 @@ package "hammertest" where
 lean_lib «Hammertest» where
   -- add library configuration options here
 
-require auto from "../lean-auto/"
+require auto from git "https://github.com/Louddy/lean-auto-tests.git" @ "main"
 
-require Duper from "../duper/"
+require Duper from git "https://github.com/leanprover-community/duper.git" @ "hammer"
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+  "https://github.com/leanprover-community/mathlib4.git" @ "v4.20.0"
 
 @[default_target]
 lean_exe "hammertest" where
