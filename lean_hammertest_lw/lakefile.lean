ee9db335405d331fecbc0d2ba75175505d27436a
Xavier Genereux <xaviergenereux@hotmail.com>
2025-09-18 15:39:53 +0200

    update lakefile

--- lean_hammertest_lw/lakefile.lean
+++ lean_hammertest_lw/lakefile.lean
@@ -6,7 +6,7 @@ package hammertest
 @[default_target]
 lean_lib Hammertest
 
-require auto from git "https://github.com/Louddy/lean-auto-tests.git" @ "v4.20-new-forward-unsafe"
+require auto from git "https://github.com/Louddy/lean-auto-tests.git" @ "unify-tests"
 
 require Duper from git "https://github.com/leanprover-community/duper.git" @ "hammer"
 
