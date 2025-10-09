ed88d7622ecbec8d18d655142c9d687e98d50378
Xavier Genereux <xaviergenereux@hotmail.com>
2025-10-08 20:00:30 -0400

    create saturate branch

--- lean_hammertest_lw/lakefile.lean
+++ lean_hammertest_lw/lakefile.lean
@@ -6,7 +6,7 @@ package hammertest
 @[default_target]
 lean_lib Hammertest
 
-require auto from git "https://github.com/Louddy/lean-auto-tests.git" @ "unify-tests"
+require auto from git "https://github.com/Louddy/lean-auto-tests.git" @ "saturate"
 
 require Duper from git "https://github.com/leanprover-community/duper.git"
   @ "f444c7ece7df389b5c80e003b525dbc1a88844f5"
