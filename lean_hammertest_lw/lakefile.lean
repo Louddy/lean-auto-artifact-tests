af95aaed23a4d0bcac2ba5ff0aad1056a2920807
Xavier Genereux <xaviergenereux@hotmail.com>
2025-10-13 19:17:55 +0200

    fix lake

--- lean_hammertest_lw/lakefile.lean
+++ lean_hammertest_lw/lakefile.lean
@@ -6,7 +6,7 @@ package hammertest
 @[default_target]
 lean_lib Hammertest
 
-require auto from git "https://github.com/Louddy/lean-auto-tests.git" @ "unify-tests"
-
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @ "v4.20.0"
+
+require auto from git "https://github.com/Louddy/lean-auto-tests.git" @ "unify-tests"
