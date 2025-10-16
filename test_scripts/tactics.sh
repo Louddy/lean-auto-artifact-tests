777f9abfe93095d4007ffd5bc0721ad8c083864f
Xavier Genereux <xaviergenereux@hotmail.com>
2025-10-16 11:39:38 +0200

    add nonterminates
    add some slow theorems (more than 10 min) from lazy to nonterminates

--- test_scripts/tactics.sh
+++ test_scripts/tactics.sh
@@ -56,6 +56,18 @@ set_option auto.testTactics.ensureAesop true
           \`\`IntermediateField.extendScalars_inf,
           \`\`Field.Emb.Cardinal.succEquiv_coherence,
           \`\`UniformConvergenceCLM.uniformSpace_eq,
+          \`\`Module.flat_of_localized_span,
+          \`\`AlgebraicGeometry.Ideal.span_eq_top_of_span_image_evalRingHom,
+          \`\`multiplicity_eq_zero_of_coprime,
+          \`\`Cardinal.mk_subset_ge_of_subset_image,
+          \`\`Module.mem_support_iff',
+          \`\`ModuleCat.Tilde.sections_smul_localizations_def,
+          \`\`countable_image_gt_image_Ioi,
+          \`\`Ideal.IsHomogeneous.isPrime_of_homogeneous_mem_or_mem,
+          \`\`Matrix.matPolyEquiv_charmatrix,
+          \`\`injective_zsmul_iff_not_isOfFinAddOrder,
+          \`\`Module.Flat.of_shrink,
+          \`\`LinearMap.dualMap_bijective_iff,
         ]
         tactics.flatMap fun tac => decls.map fun decl => (tac, decl)
       nprocs := $num_procs
